01e907536c2f10ed16500250e9c51bae80e42ecf
CollinBarlow <collinbarlow@gmail.com>
2023-04-16 13:10:20 -0700

    order model created

---
+++ fish-market/models/order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const fishSchema = require('./fish');
+const Schema = mongoose.Schema;
+
+const lineItemSchema = new Schema ({
+    quantity: {type: Number, default: 1},
+    item: fishSchema,
+},{
+    timestamps: true,
+    toJSON: {virtuals: true}
+})
+
+lineItemSchema.virtual('extPrice').get(function() {
+    return this.quantity*this.item.price
+})
+
+const orderSchema = newSchema ({
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    lineItems: [lineItemSchema],
+    isPaid: {type: Boolean, default: false}
+}, {
+    timestamps: true,
+    toJSON: {virtuasl: true}
+});
+
+//need to add virtuals to order documents for cart
+
+
+module.exports = mongoose.model('Order', orderSchema);
