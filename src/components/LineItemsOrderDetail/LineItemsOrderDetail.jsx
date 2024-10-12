8faa8ef30a933fa7084bc67c1bc42146df3aede3
CollinBarlow <collinbarlow@gmail.com>
2024-10-11 23:09:36 -0700

    need to finesse cart card styling

--- src/components/LineItemsOrderDetail/LineItemsOrderDetail.jsx
+++ src/components/LineItemsOrderDetail/LineItemsOrderDetail.jsx
@@ -6,16 +6,26 @@ export default function LineItemsOrderDetail({ lineItem, handleChangeQty }) {
   if (lineItem.quantity > 0) {
     return (
         <div className="cartFishOrderCard">
+        <div className="cartFishOrderCardWrapper">
+        <div className="cartFishOrderCardWrapper2">
           <div className="cartFishOrderCardImage">
             <img src={lineItem.item.imgUrl} />
           </div>
           <div className="cartFishOrderCardInfo">
-            <div className="cartIndividualMetrics"> Name: {lineItem.item.speciesName}</div>
-            <div className="cartIndividualMetrics"> Quantity: {lineItem.quantity}</div>
-            <div className="cartIndividualMetrics"> Price: ${lineItem.extPrice}</div>
+            <div className="cartIndividualMetrics"> {lineItem.item.speciesName}</div>
+            <div className="cartIndividualMetrics"> 
+            <div>Quantity: </div>
+            <div>{lineItem.quantity}</div>
+            </div>
+            <div className="cartIndividualMetrics"> 
+            <div>Price: </div>
+            <div>${lineItem.extPrice}</div>
+            </div>
             <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity + 1)}>Add</button>
             <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity - 1)}>Remove</button>
           </div>
+        </div>
+        </div>
         </div>
     )
   } else {
