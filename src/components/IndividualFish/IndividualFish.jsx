84136669310211dc9824c79facec8f55c64e7711
CollinBarlow <collinbarlow@gmail.com>
2024-10-14 14:33:03 -0700

    implemented css from cart to product

--- src/components/IndividualFish/IndividualFish.jsx
+++ src/components/IndividualFish/IndividualFish.jsx
@@ -3,19 +3,27 @@ import './IndividualFish.css'
 import { Link } from 'react-router-dom'
 
 export default function IndividualFish({ fishes, handleAddToOrder, showFishComponent, setShowFishComponent, handleShowFish }) {
+  // console.log(fishes)
 
   function FishDetail() {
     handleShowFish(setShowFishComponent)
   }
 
   return (
-    <div className='fishCardForeGround'>
-      <img src={fishes.imgUrl} />
-      <div>Name: <span>{fishes.speciesName} </span>
+    <div className='cartFishOrderCard'>
+      <div className="cartFishOrderCardWrapper">
+        <div className="cartFishOrderCardWrapper2">
+          <div className="cartFishOrderCardImage">
+            <img src={fishes.imgUrl} />
+          </div>
+          <div className="cartFishOrderCardInfo">
+            <div className="cartIndividualMetrics"> {fishes.speciesName}</div>
+            <div className="cartIndividualMetrics"> <div>Price:</div><div> ${fishes.price}/lb</div></div>
+            <button className="cartButtons" onClick={() => handleAddToOrder(fishes._id)}>Add to cart</button>
+            <button className="cartButtons"> <Link to={`/fish/${fishes.speciesName}`} onClick={FishDetail}>Detail Page</Link></button>
+          </div>
+        </div>
       </div>
-      <div>Price: ${fishes.price} per fish</div>
-      <div><button onClick={() => handleAddToOrder(fishes._id)}>Add to cart</button>
-        <Link to={`/fish/${fishes.speciesName}`} onClick={FishDetail}>Detail Page</Link> </div>
     </div>
   )
 }
--- src/components/LineItemsOrderDetail/LineItemsOrderDetail.jsx
+++ src/components/LineItemsOrderDetail/LineItemsOrderDetail.jsx
@@ -5,28 +5,28 @@ export default function LineItemsOrderDetail({ lineItem, handleChangeQty }) {
 
   if (lineItem.quantity > 0) {
     return (
-        <div className="cartFishOrderCard">
+      <div className="cartFishOrderCard">
         <div className="cartFishOrderCardWrapper">
-        <div className="cartFishOrderCardWrapper2">
-          <div className="cartFishOrderCardImage">
-            <img src={lineItem.item.imgUrl} />
-          </div>
-          <div className="cartFishOrderCardInfo">
-            <div className="cartIndividualMetrics"> {lineItem.item.speciesName}</div>
-            <div className="cartIndividualMetrics"> 
-            <div>Quantity: </div>
-            <div>{lineItem.quantity}</div>
+          <div className="cartFishOrderCardWrapper2">
+            <div className="cartFishOrderCardImage">
+              <img src={lineItem.item.imgUrl} />
             </div>
-            <div className="cartIndividualMetrics"> 
-            <div>Price: </div>
-            <div>${lineItem.extPrice}</div>
+            <div className="cartFishOrderCardInfo">
+              <div className="cartIndividualMetrics"> {lineItem.item.speciesName}</div>
+              <div className="cartIndividualMetrics">
+                <div>Quantity: </div>
+                <div>{lineItem.quantity}</div>
+              </div>
+              <div className="cartIndividualMetrics">
+                <div>Price: </div>
+                <div>${lineItem.extPrice}</div>
+              </div>
+              <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity + 1)}>Add</button>
+              <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity - 1)}>Remove</button>
             </div>
-            <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity + 1)}>Add</button>
-            <button className="cartButtons" onClick={() => handleChangeQty(lineItem.item._id, lineItem.quantity - 1)}>Remove</button>
           </div>
         </div>
-        </div>
-        </div>
+      </div>
     )
   } else {
     return null;
